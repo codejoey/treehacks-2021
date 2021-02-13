839b8c9e9fc84a77bec0483cce40126a4f67aaa9
rasfmar <38634617+rasfmar@users.noreply.github.com>
2021-02-13 00:29:32 -0500

    added process page boilerplate

---
+++ packages/react-app/src/views/Process.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+import React, { useEffect } from "react";
+
+export default function Process() {
+  return (
+    <div>
+      test
+    </div>
+  );
+}
