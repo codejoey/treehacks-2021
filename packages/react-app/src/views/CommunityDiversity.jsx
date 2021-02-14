2a10840f89f70bcf5fc2660b5e0a08b1afcc5e1e
rasfmar <38634617+rasfmar@users.noreply.github.com>
2021-02-14 03:39:34 -0500

    ready storymap pages

---
+++ packages/react-app/src/views/CommunityDiversity.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+import React from "react";
+
+export default function CommunityDiversity() {
+  return (
+    <iframe
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none"
+      }}
+      src="https://storymaps.arcgis.com/stories/e9bc0b2fed02426fbbded98a6a640d09"
+    />
+  );
+}
